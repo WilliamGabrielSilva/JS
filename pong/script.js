c8c442895e789b280fe93b8a879aa98276260d82
William Gabriel Silva <127068960+WilliamGabrielSilva@users.noreply.github.com>
2023-06-19 21:49:53 -0300

    khan cademy

---
+++ pong/script.js
@@ -0,0 +1,6 @@
+let bolinha = document.getElementById("bolinha");
+let raquete1 = document.getElementById("raquete1");
+let raquet2 = document.getElementById("raquete2")
+
+//movimento da bolinha 
+
